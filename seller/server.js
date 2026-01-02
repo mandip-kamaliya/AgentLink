10cdee6c145051080d2fe35ce923762e306c6886
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2026-01-02 10:34:28 +0530

    server.js file is added

--- seller/server.js
+++ seller/server.js
@@ -4,8 +4,21 @@ const cors = require('cors');
 const ethers = require('ethers');
 const openai = require('openai');
 
+
+if(!process.env.SELLER_WALLET){
+    console.log("server wallet is missing");
+}
+
+
 const app=express();
+app.use(cors());
+app.use(express.json());
+
 const PORT = 3000;
+const SELLER_WALLET = process.env.SELLER_WALLET;
+const PRICE = "0.01";
+const PROVIDER_URL = "https://evm-t3.cronos.org";
+const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
 
 app.listen(PORT,()=>{
     console.log(`🟢 AGENT-LINK PRO SERVER ONLINE (${PORT})`)
